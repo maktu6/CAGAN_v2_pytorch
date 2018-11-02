009f05ebedcf6b0a9edcc2a21caac605af9543dc
Maktu6 <maktu6@163.com>
2018-11-02 16:34:24 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# CAGAN_v2_pytorch
+A PyTorch implementation of [CAGAN](https://arxiv.org/abs/1709.04695) combines with [StackGAN-v2](https://arxiv.org/abs/1710.10916)  
+Code based on the [original implementation in keras](https://github.com/shaoanlu/Conditional-Analogy-GAN-keras)  
+The following repositories were also referenced:   
+- [https://github.com/hanzhanggit/StackGAN-v2](https://github.com/junyanz/pytorch-CycleGAN-and-pix2pix)
+- [https://github.com/junyanz/pytorch-CycleGAN-and-pix2pix](https://github.com/junyanz/pytorch-CycleGAN-and-pix2pix)  
+
+## Prerequisites
+- Python 3.6
+- PyTorch 0.4.0
+- Visdom
+
+## Description
+### Data
+The dataset is just same as [here](https://github.com/shaoanlu/Conditional-Analogy-GAN-keras/blob/master/README.md#description) and the folder of dataset is structured as follows: 
+```
+├── data
+    ├── imgs_test
+    │   ├── 1
+    │   └── 5
+    └── imgs_train
+        ├── 1
+        └── 5
+
+```
+
+### Train
+- Open a visdom server  
+```bash
+python -m visdom.server
+```
+- Train a model  
+```bash
+python main.py
+```
+
+### Test
+Use trained model to generate images and calculate IS score  
+```bash
+python main.py --mode test --model_dir logs/origin/model_weight
+```
+
---
+++ main.py
@@ -0,0 +1,83 @@
+import torch
+from torch.utils import data
+import os
+import argparse
+from utils import trainer, visualizer, cagan_dataset, options
+
+opt = options.GatherOptions().parse()
+if opt.mode == "train":
+    train_dataset = cagan_dataset.CAGAN_Dataset(opt.data_root)
+    train_dataloader = data.DataLoader(train_dataset, opt.batchsize, shuffle=True,
+                                    num_workers=opt.num_workers, pin_memory=True)
+    # calculate num of steps for decaying learning to zero at the end of training
+    if (opt.lr_policy == 'lambda') and (opt.niter_decay == -1):
+        opt.niter_decay = opt.epoch*len(train_dataloader)*opt.batchsize - opt.niter
+    model = trainer.CAGAN_Trainer(opt, save_dir=opt.save_dir)
+
+    vis_custom = visualizer.Visualizer(opt.mode)
+    vis_custom.reset_env()
+    loss_save_path = os.path.join(opt.save_dir, 'loss.dict')
+    step = 0
+    if opt.resume:
+        step = opt.step
+        model.load_networks(step=step, load_netD=True)
+        try:
+            vis_custom.recover_loss(loss_save_path)
+        except:
+            print("Loss dict can not be found in %s"%opt.save_dir)
+
+    for epoch in range(opt.epoch):
+        for real in train_dataloader:
+            step += 1
+            model.set_input(real)
+            model.optimize_parameters()
+
+            loss_dict =model.get_current_losses()
+            loss_str = '[step %d/epoch %d]'%(step, epoch+1)
+            for key, data in loss_dict.items():
+                if "sum" in key:
+                    loss_str += key + ': ' + '%.3f'%(data) + ' '
+            print(loss_str[:-2])
+            vis_custom.plot_current_losses(step, loss_dict)
+            vis_custom.plot_current_images(model.get_current_visuals(), 'real-fake-rec-alpha')
+            if step % opt.save_image_freq == 0:
+                model.save_current_visuals(step, opt.batchsize)
+            if step % opt.save_model_freq == 0:
+                model.save_networks(step)
+                torch.save(vis_custom.plot_data, loss_save_path)
+            if opt.lr_policy in ['lambda']:
+                model.update_learning_rate()
+        if opt.lr_policy not in ['lambda']:
+            model.update_learning_rate()
+
+    model.save_current_visuals(step, opt.batchsize)
+    model.save_networks(step)
+else:
+    inception_model = IS_score.INCEPTION_V3()
+    inception_model.eval()
+    inception_model.to('cuda')
+    test_dataset = cagan_dataset.CAGAN_Dataset(opt.data_root, mode='test')
+    test_dataloader = data.DataLoader(test_dataset, opt.batchsize, shuffle=False,
+                                    num_workers=opt.num_workers, pin_memory=True)
+    model = trainer.CAGAN_Trainer(opt, is_train=False)
+    step = None
+    if opt.step != -1:
+        step = opt.step
+    model.load_networks(step=step, load_dir=opt.model_dir)
+    vis_custom = visualizer.Visualizer('test')
+    vis_custom.reset_env()
+    predictions = []
+    step = 0
+    for real in test_dataloader:
+        step += 1
+        model.set_input(real)
+        with torch.autograd.no_grad():
+            model.netG_forward()
+            pred = inception_model(model.output_dict['fake_outputs'][-1])
+        predictions.append(pred.data.cpu().numpy())
+        vis_custom.plot_current_images(model.get_current_visuals(),
+                                    'real-fake-rec-alpha', 
+                                    len(real), step)
+    predictions = np.concatenate(predictions, 0)
+    mean, std = IS_score.compute_inception_score(predictions, 10)
+    print('IS score --- mean: %.4f, std: %.4f'%(mean, std))
